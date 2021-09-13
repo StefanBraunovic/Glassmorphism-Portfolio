e854c0f914066c849729aa4d3b105eeeeae9ed39
Stefan Braunovic <stefan.braunovic55@gmail.com>
2021-09-13 15:21:47 +0200

    header/navbar section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+// Toggle Navbar
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", ()=>{
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+})
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
+
+// Active Section
+document.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        // active the overlay to prevent multiple clicks
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+          toggleNavbar();
+        }
+        else{
+           hideSection();
+           document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add('active');
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500)
+    }
+})
+
+
+
 // About Tabs
 
 const tabsContainer = document.querySelector(".about-tabs"),
@@ -57,3 +98,6 @@ portfolioItem.querySelector(".portfolio-item-tittle").innerHTML;
 document.querySelector(".pp-body").innerHTML=
 portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
+
+
+// Toggle Navbar
